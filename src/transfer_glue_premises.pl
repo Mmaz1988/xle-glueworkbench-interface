68340de5bbf7f5a9e837aa44ca697aeb7d77bca4
Agnieszka Patejuk <aep@ipipan.waw.pl>
2020-08-24 15:29:00 +0100

    rename REL attribute to MR

--- src/transfer_glue_premises.pl
+++ src/transfer_glue_premises.pl
@@ -7,6 +7,7 @@
 /* and transfer them to the input format for the prover.                    */
 /* ------------------------------------------------------------------------ */
 
+:- use_module(library(pcre)). % regular expression library
 :- ensure_loaded(extract_analysis).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -61,54 +62,100 @@ collect_premise_ids(_FStr, [], []).
 % expected by the prover.
 extract_premises(FStr, [PremiseID|PremiseIDs],[Premise|Premises]) :-
     convert_fstr_to_premise(FStr, PremiseID, Premise),
-    extract_premises( FStr, PremiseIDs, Premises).
+    extract_premises(FStr, PremiseIDs, Premises).
 extract_premises(_FStr, [], []).
 
 convert_fstr_to_premise(FStr, PremiseID, Premise) :-
     % Find all of the attributes and values of the f-structure PremiseID.
     bagof([Attr,Val], find_attr(FStr, PremiseID, Attr, Val), AttrVals),
-    % Find the meaning side.  This will be the value of the REL attribute.
-    member(['REL',Meaning], AttrVals),
-    % Find the glue side. 
-    find_glue([], FStr, AttrVals, Glue),
-    % Assemble the premise in the form that the prover expects.
-    atomics_to_string([Meaning, " : ", Glue], Premise).
+    ( % This is the case where the meaning constructor is defined in terms
+      % of embedding, and we will find the distinguished attribute
+      % MR (this was REL in former versions of the implementation).
+      % The meaning side will be the value of the MR attribute.
+      member(['MR', RelVal], AttrVals), !,
+      % Find the glue side. 
+      find_glue([], FStr, AttrVals, Glue),
+      % Assemble the meaning side.
+      assemble_meaning_expression(FStr, RelVal, Meaning),
+      % Assemble the premise in the form that the prover expects.
+      atomics_to_string([Meaning, " : ", Glue], Premise) ;
+      % This is the case where there is no MR attribute.  We are
+      % using the concatenative encoding, and we just pass the
+      % f-structure to a component that concatenates the parts.
+      concatenate_premise(AttrVals, Premise) ).
 
-% Assemble the glue side of the meaning constructor.  First argument is either the empty set of variables
-% (we are not in the scope of a quantifier binding a variable) or a variable bound by a quantifier,
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% These functions are used for premises encoded with MR, TYPE,
+% SEMSTR, and FORALL in the embedded format.
+
+% Assemble the meaning expression. This will usually just be the value 
+% of the MR attribute, but it could be a pointer to it. [New, 16 June 2020]
+assemble_meaning_expression(_FStr, Meaning, Meaning) :-
+    atomic(Meaning).
+assemble_meaning_expression(FStr, RelVal0, Meaning) :-
+    find_attr(FStr, RelVal0, equal, RelVal),
+    assemble_meaning_expression(FStr, RelVal, Meaning).
+
+% Assemble the glue side of the meaning constructor.  First argument
+% is either the empty set of variables (we are not in the scope of a
+% quantifier binding a variable) or a variable bound by a quantifier,
 % which has to be prefixed with "F".
 find_glue(BoundVars0, FStr, AttrVals, Glue) :-
-    check_for_quant(AttrVals, BoundVar),
-    append(BoundVar, BoundVars0, BoundVars),
+    % Check for quantification via the FORALL attribute
+    check_for_quant(AttrVals, FStr, BoundVars1),
+    append(BoundVars0, BoundVars1, BoundVars),
     % Assemble the result term from the attributes SEMSTR and TYPE.
     assemble_term(BoundVars, AttrVals, Result),
-    % Check for quantification via the FORALL attribute
-    % Construct the argument terms.
+    % Construct the argument terms.  If the formula is in the scope of
+    % a quantified variable or a set of quantified variables, prefix
+    % all occurrences of the variable(s) in the string with "F".
     msort(AttrVals, AttrValsSorted),
     find_glue_args(BoundVars, FStr, AttrValsSorted, Args),
-    % Assemble the result.
+    % Assemble the result.  
     assemble_glue_expression(Args, Result, Glue0),
-    % Add quantification over the s-structure as a prefix.  Quant is the empty
-    % list if check_for_quant found no quantifier.  If there is a quantified variable, prefix
-    % all occurrences of the variable in the string with "F".
-    add_quant(BoundVar, Glue0, Glue).
+    % Add quantifiers for the values of the FORALL attribute as a
+    % prefix.  BoundVars1 is the empty list if check_for_quant found no
+    % quantifier.
+    add_quant(BoundVars1, Glue0, Glue).
 
-% Check for a quantifier over s-structures. If the scope is a semantic form, prefix "s"
-% to distinguish it from f-structures with the same numeric identifier.
-check_for_quant(AttrVals, [var(V)]) :-
-    ( member(['FORALL',var(V)], AttrVals);
-      member(['FORALL',semform(_F,SV,_A,_NA)], AttrVals),
-      atom_concat('s', SV, V)), !.
-check_for_quant(_AttrVals, []).
+% Check for a quantifier over s-structures.  Assume that the scope might be a PRED value.
+% It is possible for the value of FORALL to be a set.
+check_for_quant(AttrVals, FStr, QuantifiedFstrs) :-
+    member(['FORALL',Val], AttrVals), !, % There are some quantified variables.
+    ( Val = semform(_F,V,_A,_NA), QuantifiedFstrs = [quant(V,_Type)] ; % The value of FORALL is a PRED value
+      Val = var(V),
+        ( % Collect the IDs of each member of the set value of the
+          % attribute 'FORALL'.
+	    bagof(Member, find_attr(FStr, var(V), member, Member), [M|Members]),
+	    % Check whether each of the VALS is a regular f-structure
+	    % or a PRED, and rewrite the PREDs to quant(V,Type) format
+	    normalize([M|Members], QuantifiedFstrs) ; 
+	  % If the user has not made 'FORALL' a set-valued attribute,
+	  % assume that the value of 'FORALL' is a single structure,
+	  % and return the normalized value of the 'FORALL' attribute as a
+	  % single-membered list 
+	    normalize([Val], QuantifiedFstrs) )).
+check_for_quant(_AttrVals, _FStr, []). % There are no quantifiers.
 
-% Add the quantifier prefix.
-add_quant([], Glue, Glue). % There is no quantifier prefix.
-add_quant([var(V)], Glue0, Glue) :-
-   atomics_to_string(["AF", V, "_t.", Glue0], Glue).
+% Rewrite semantic forms to structures encoding the quantified structure and its type.
+% Type is still uninstantiated at this point, but will be instantiated in the course of
+% constructing the meaning term.
+normalize([], []).
+normalize([var(V)|Rest0], [quant(V,_Type)|Rest]) :-
+	      normalize(Rest0, Rest).
+normalize([semform(_F,V,_A,_NA)|Rest0], [quant(V,_Type)|Rest]) :-
+	      normalize(Rest0, Rest).    
+
+% Add the quantifier prefix.  There may be more than one quantifier, if the value of FORALL
+% is a set.
+add_quant([], Quant, Quant). % There are no quantifiers.
+add_quant([quant(V,Type)|Rest], Quant0, Quant) :-
+    atomics_to_string(["AF", V, "_", Type, ".", Quant0], Quant1),
+    add_quant(Rest,Quant1,Quant).
 
 find_glue_args(BoundVars, FStr, [[Attr,_]|ArgAttrVals], Args):-
-    % Ignore the REL, SEMSTR, TYPE, and FORALL attributes.  
-    member(Attr, ['REL','SEMSTR','TYPE','FORALL']), !,
+    % Ignore the MR, SEMSTR, TYPE, and FORALL attributes.  
+    member(Attr, ['MR','SEMSTR','TYPE','FORALL']), !,
     find_glue_args(BoundVars, FStr, ArgAttrVals, Args).
 find_glue_args(BoundVars, FStr, [[_Arg,PremiseID]|ArgAttrVals], [Arg|Args]):-
     % These are the arguments ARG1 through ARGN, already sorted by msort above.
@@ -126,23 +173,52 @@ assemble_glue_expression([Arg|Args], G, Premise) :-
     atomics_to_string(["(", Arg, " -o ", Premise0, ")"], Premise).
 
 % Assemble a term: the f-structure ID stands in for the semantic structure, and the
-% type is the value of TYPE.  If V is an identifier for a semantic form, prefix "s".
-% If V is a bound variable, prefix "F".
+% type is the value of TYPE.  If V is a bound variable, prefix "F".
 assemble_term(BoundVars, AttrVals, Term) :-
     % find a glue atom and its type corresponding to PremiseID, create term
     ( member(['SEMSTR',var(V)], AttrVals);
-      member(['SEMSTR',semform(_F,SV,_A,_NA)], AttrVals),
-      atom_concat('s', SV, V) ),
+      member(['SEMSTR',semform(_F,V,_A,_NA)], AttrVals) ),
     member(['TYPE',Type], AttrVals),
-    ( member(var(V), BoundVars), !,  atomics_to_string(['F', V, '_', Type], Term);
+    ( member(quant(V,Type), BoundVars), !,  atomics_to_string(['F', V, '_', Type], Term);
       atomics_to_string([V, '_', Type], Term) ).
       
-
-% F-structure FID has attributes Attr with value Val. 
+% F-structure FID has attributes Attr with value Val, or f-structure FID declared equal to Val. 
 find_attr([cf(_,eq(attr(FID,Attr),Val))|_FStr], FID, Attr, Val).
+find_attr([cf(_,eq(FID,Val))|_Fstr], FID, equal, Val). % NEW, 16 June 2020
 find_attr([cf(_,in_set(Val,FID))|_FStr], FID, member, Val).
 find_attr([_|FStr], FID, Attr, Val) :-
     find_attr(FStr, FID, Attr, Val).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% These functions are used for premises encoded in the string-
+% based format.  We just concatenate the parts together.
+
+concatenate_premise(AttrVals, Premise) :-
+    msort(AttrVals, AttrValsSorted),
+    list_components(AttrValsSorted, Atomics),
+    atomics_to_string(Atomics, Premise0),
+    premise_cleanup(Premise0, Premise).
+
+% Each member of the list is an attribute-value pair where the value is
+% either a string or an f-structure reference.
+list_components([AttrVal|AttrVals], [String|Atomics]) :-
+    convert_premise_piece(AttrVal, String),
+    list_components(AttrVals, Atomics).
+list_components([], []).
+
+convert_premise_piece([_Attr, var(V)], V) :- !.
+convert_premise_piece([_Attr, semform(_F,V,_A,_NA)], V) :- !.
+convert_premise_piece([_Attr, String], String).
+
+% Clean up the string resulting from concatenating together the parts.
+% Underscores in the f-structure are converted to double underscores in the
+% XLE Prolog output, so these need to be converted back to single underscores.
+% Linear implication should be surrounded by whitespace.
+% The flag "/g" says to replace all occurrences.
+premise_cleanup(Premise0, Premise) :-
+    re_replace('__'/g, '_', Premise0, Premise1),
+    re_replace('-o'/g, ' -o ', Premise1, Premise).
+  
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Clean up the premises by removing duplicate premise sets, which could arise from syntactic
